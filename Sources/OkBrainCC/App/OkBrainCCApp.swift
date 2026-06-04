69d173293313d32faed5a6b15d5485a80c7729ed
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2026-06-05 03:00:04 +0530

    Making sure this is still a menubar app

--- Sources/OkBrainCC/App/OkBrainCCApp.swift
+++ Sources/OkBrainCC/App/OkBrainCCApp.swift
@@ -15,7 +15,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
   }
 
   func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    true
+    false
   }
 
   func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
